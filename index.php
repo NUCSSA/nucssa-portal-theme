77371851183b0cc5921ffb0dcb6b805b0c779385
Jason Ji <lu.ji1@me.com>
2019-08-23 22:04:11 -0400

    Add Blog View Page Template. Resolves #10

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-use Timber\Timber;
+use \Timber\Timber;
+use \Timber\PostQuery;
 $context = Timber::context();
+$context['posts'] = new PostQuery();
 
 Timber::render('index.twig', $context);
