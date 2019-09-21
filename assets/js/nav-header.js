8e84fe2ccffe0faceabd0597d409d37175fd77e5
Jason Ji <lu.ji1@me.com>
2019-09-21 06:41:56 -0400

    off canvas nav panel

--- assets/js/nav-header.js
+++ assets/js/nav-header.js
@@ -5,26 +5,6 @@ import { disablePageScroll, enablePageScroll } from './utils';
   document.addEventListener('DOMContentLoaded', () => {
 
     /**
-     * Hamburger
-     */
-    const navPanel = document.querySelector('#nav-panel');
-    const navToggle = document.querySelector('header .nav-toggle');
-    const toggleNavPanel = () => {
-      // remove or add `active` class to navToggle button
-      const isActive = navToggle.classList.toggle('active');
-      // show or hide navPanel
-      navPanel.classList.toggle('active', isActive);
-      // disable or enable page scrolling
-      if (isActive) {
-        disablePageScroll();
-      } else {
-        enablePageScroll();
-      }
-    };
-    navToggle.addEventListener('click', toggleNavPanel);
-
-
-    /**
      * Search on focus
      */
     const searchBar = document.querySelector('#globalnav .nav-group.searchbar');
