19cf0d6da618da3345bbfe6a63b2d7465cb4877b
Lu Ji <lu.ji1@me.com>
2021-04-09 19:17:59 -0400

    1. refactor clubs and coupons page:        convert them from page templates to respective archive page.        This way, we avoid having empty pages pointing templates, that might confuse editors. 2. remove obsolete code in index.php 3. update class names in scss files for the refactoring in step 1.

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-use \Timber\Timber;
-use \Timber\PostQuery;
-$context = Timber::context();
-$context['posts'] = new PostQuery();
+/**
+ * Since we have front-page.php (for front page), home.php (for posts archive) and other template files covering all requests, 
+ * rendering this index file obsolete. 
+ * But we'll keep this as a catchall, in case there is a case our templates files doesn't cover. 
+ * Simply redirect to home
+ */
+wp_redirect( home_url(), 302 );
+exit;
 
-Timber::render('index.twig', $context);
